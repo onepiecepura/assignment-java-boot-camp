0fbd063232e396310c3416c962995047640ebcb6
Chaiwat `Aof` Ratchawat <chaiwat.r@kaidee.com>
2022-02-26 15:18:07 +0700

    feat: setup spring boot project #2

--- week01/src/test/java/com/example/week01/ShoppingApplicationTests.java
+++ week01/src/test/java/com/example/week01/ShoppingApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Week01ApplicationTests {
-
+class ShoppingApplicationTests {
 	@Test
+
 	void contextLoads() {
 	}
 
