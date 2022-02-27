73fcfc0da03cf2659622779aba5f91b2b60d4be8
Chaiwat `Aof` Ratchawat <chaiwat.r@kaidee.com>
2022-02-27 16:52:13 +0700

    feat: create product controller #1

---
+++ src/main/java/com/example/week01/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.week01.product;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ProductsResponse searchProducts(@RequestParam String name){
+        return productService.searchByName(name);
+    }
+
+}
