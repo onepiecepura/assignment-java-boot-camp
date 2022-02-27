d5d6abcfe4a95cb2a43e8b799f979821d3c9fc90
Chaiwat `Aof` Ratchawat <chaiwat.r@kaidee.com>
2022-02-27 18:36:38 +0700

    feat: find product by id #3

--- src/main/java/com/example/week01/product/ProductController.java
+++ src/main/java/com/example/week01/product/ProductController.java
@@ -3,6 +3,7 @@ package com.example.week01.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,8 @@ public class ProductController {
         return productService.searchByName(name);
     }
 
+    @GetMapping("/products/{id}")
+    public ProductDetailResponse getProductById(@PathVariable Integer id){
+        return  productService.findById(id);
+    }
 }
--- src/main/java/com/example/week01/product/ProductService.java
+++ src/main/java/com/example/week01/product/ProductService.java
@@ -3,6 +3,8 @@ package com.example.week01.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProductService {
 
@@ -11,6 +13,11 @@ public class ProductService {
 
     public ProductsResponse searchByName(String name){
         return new ProductsResponse(productRepository.findProductByNameContains(name));
+    }
+
+    public ProductDetailResponse findById(Integer id){
+        Optional<Product> product = productRepository.findById(id);
+        return new ProductDetailResponse(product.get());
     }
 
 }
